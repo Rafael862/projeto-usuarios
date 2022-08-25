789375af4415214b982b654a30325bd0ed165f73
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-08-24 21:32:49 -0300

    learn MVC

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+let userController = new UserController("form-user-create", "table-users");
 
 
 
-
-
-var fields = document.querySelectorAll("#form-user-create [name]");
-
-fields.forEach(function(field, index){
-
-    if (field.name == "gender"){
-        if(field.checked){
-        console.log('SIM', field);
-
-        }
-
-
-    } else {
-        console.log('NAO');
-    }
-
-    //console.log(field);
-    
-
-});
